f127fc6f8d514870c983cf734cd3703936ee4897
Oscar Cardenas <cardenas97@MacBook-Pro-de-Oscar.local>
2023-08-22 18:37:50 -0600

    app.js

--- proyecto/proyecto-api/app.js
+++ proyecto/proyecto-api/app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const jwt = require('jsonwebtoken');
+const db= require('./models');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//set jwt secret
+app.set('secretKey', 'xxCursodeReactxx');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +22,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//handle errors
+app.use(function(err, req, res, next){
+    console.log(err);
+    if(err.status == 404){
+        res.status(404).json({message: 'Not found'});
+    }else{
+        res.status(500).json({message: "Something looks wrong :(", error: err})
+    }
+})
+
+//middlewares
+function validateUser(req, res, next){
+    jwt.verify(req.headers('x-access-token'), req.app.get('secretKey'), function(err, decoded){
+        if(err){
+            req.body.error = err.message;
+            res.status(401).json({status:"error", message: err.message, data:null})
+        }else{
+            req.body.userId = decoded.id;
+            next();
+
+        }
+    })
+}
 
 module.exports = app;
